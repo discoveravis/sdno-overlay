40e5d83ea1616663c84a29bb0000f191ce40fd8c
Kanagaraj Manickam k00365106 <kanagaraj.manickam@huawei.com>
2016-09-09 03:09:39 +0530

    Add extra in AuthInfo
    For openstack, we need to add region and domain
    which is added using new param called 'extra'
    in AuthInfo.
    
    Change-Id: I72005df56ded63f73055519ca85d47d15aea174c
    Signed-off-by: Kanagaraj Manickam <kanagaraj.manickam@huawei.com>

--- cbb/src/main/java/org/openo/sdno/overlayvpn/brs/model/AuthInfo.java
+++ cbb/src/main/java/org/openo/sdno/overlayvpn/brs/model/AuthInfo.java
@@ -16,6 +16,9 @@
 
 package org.openo.sdno.overlayvpn.brs.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Authentication info class.<br/>
  * 
@@ -31,6 +34,8 @@ public class AuthInfo {
     private String port;
 
     private String transProtocol;
+
+    private Map<String, String> extra = new HashMap<>();
 
     public String getUserName() {
         return userName;
@@ -63,5 +68,13 @@ public class AuthInfo {
     public void setTransProtocol(String transProtocol) {
         this.transProtocol = transProtocol;
     }
+
+   public Map<String, String> getExtra() {
+      return extra;
+   }
+
+   public void setExtra(Map<String, String> extra) {
+      this.extra = extra;
+   }
 
 }
