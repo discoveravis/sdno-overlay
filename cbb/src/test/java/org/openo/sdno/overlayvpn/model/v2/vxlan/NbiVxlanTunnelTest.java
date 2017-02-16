89669e3184c80ad28a4cca336e10948e83e44285
openo-lihua <uranus.lihua@huawei.com>
2017-02-16 21:16:46 +0800

    Add UT case
    Add UT case
    
    SDNO-1172
    
    Change-Id: Iabb61aa3c6ec42106c8f6bebd18f84c4adb87fb4
    Signed-off-by: openo-lihua <uranus.lihua@huawei.com>

---
+++ cbb/src/test/java/org/openo/sdno/overlayvpn/model/v2/vxlan/NbiVxlanTunnelTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdno.overlayvpn.model.v2.vxlan;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openo.sdno.framework.container.util.JsonUtil;
+import org.openo.sdno.overlayvpn.JsonFileUtil;
+
+public class NbiVxlanTunnelTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() throws IOException {
+        String filePath = new File("src/test/resources/nbiVxlanTunnel.json").getCanonicalPath();
+        NbiVxlanTunnel nbiVxlanTunnel = JsonUtil.fromJson(JsonFileUtil.getJsonString(filePath), NbiVxlanTunnel.class);
+        assertNotNull(nbiVxlanTunnel);
+        assertNotNull(JsonUtil.toJson(nbiVxlanTunnel));
+        assertNotNull(nbiVxlanTunnel.toString());
+    }
+
+}
